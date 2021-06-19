b724e78a01f2382c98ae25628f278772455e1768
la_zorrita@ukr.net <shoblin86@gmail.com>
2021-06-19 18:26:25 +0300

    Edit index.html + creat shop_windows.html

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,20 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    post = {'title': 'Какой-то заголовок',
-            'post':''}
-    return render_template('index.html', post=post)
+    title = 'Какой-то заголовок'
+    posts = [{'title': 'Хачапури1', 'image': r'/static/img/2.jpg',
+              'description': ['Ням-Ням.', 'Вкуснота... Беги покупай']},
+             {'title': 'Хачапури2', 'image': r'/static/img/1.jpg',
+              'description': ['Ням-Ням2. Вкуснота2...', 'Беги покупай']},
+             {'title': 'Хачапури3', 'image': r'/static/img/logo.jpg',
+              'description': ['Ням-Ням3.', 'Вкуснота3... Беги покупай.', 'Варенье']},
+             {'title': 'Хачапури2', 'image': r'/static/img/logo.jpg',
+              'description': ['Ням-Ням2. Вкуснота2... Беги покупай']},
+             {'title': 'Хачапури3', 'image': r'/static/img/1.jpg',
+              'description': ['Ням-Ням3.','Вкуснота3...', 'Беги покупай']}]
+    return render_template('index.html', title=title, posts=posts)
+
+
+@app.route('/shop_window')
+def shop_window():
+    return render_template('shop_windows.html')
